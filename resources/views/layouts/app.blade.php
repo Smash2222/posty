4a43434b852edfae3ecb45a6627f56a228bb40f8
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-10-03 18:01:39 +0300

    Authenticated state, Logging in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
             <a href="" class="p-3">Home</a>
         </li>
         <li>
-            <a href="" class="p-3">Dashboard</a>
+            <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
         </li>
         <li>
             <a href="" class="p-3">Post</a>
@@ -34,18 +34,23 @@
     </ul>
 
     <ul class="flex items-center">
-        <li>
-            <a href="" class="p-3">Name</a>
-        </li>
-        <li>
-            <a href="" class="p-3">Login</a>
-        </li>
-        <li>
-            <a href="{{ route('register') }}" class="p-3">Register</a>
-        </li>
-        <li>
-            <a href="" class="p-3">Logout</a>
-        </li>
+        @auth
+            <li>
+                <a href="" class="p-3">Name</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        @endauth
+
+        @guest
+            <li>
+                <a href="{{ route('login') }}" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="{{ route('register') }}" class="p-3">Register</a>
+            </li>
+        @endguest
     </ul>
 </nav>
 
